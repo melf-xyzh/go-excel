8ef5fc46d9c146ca2edfabeb479707a785abfe7d
MELF晓宇 <xyzh.melf@petalmail.com>
2022-11-03 11:51:51 +0800

    - 添加阶梯文件导入

--- template/load.go
+++ template/load.go
@@ -92,6 +92,73 @@ func (e *ExcelConfig) LoadHttpExcel(mf multipart.File, data interface{}, ignoreR
 	return
 }
 
+// LoadHttpLadderExcel
+/**
+ *  @Description:
+ *  @receiver e
+ *  @param mf http文件对象
+ *  @param tableHead 表头
+ *  @param ignoreRows 忽略行数
+ *  @param ignoreCols 忽略列数
+ *  @param filePath 文件保存路径（不包含文件名）
+ *  @param fileName 文件名（需要保持的文件名）
+ *  @param funModule 所属功能模块名
+ *  @param userId 操作人
+ *  @param otherFunc 其他操作函数
+ *  @return fileRcd 导入导出记录
+ *  @return rows excel数据内容
+ *  @return err 错误
+ */
+func (e *ExcelConfig) LoadHttpLadderExcel(mf multipart.File, tableHead []string, ignoreRows, ignoreCols int, filePath, fileName, funModule, userId string, otherFunc func()) (fileRcd exmodel.FileRecord, rows [][]string, err error) {
+	// 读取文件内容
+	bs, err := ioutil.ReadAll(mf)
+	if err != nil {
+		err = errors.New("读取文件异常：" + err.Error())
+		return
+	}
+	// 关闭数据流
+	defer mf.Close()
+	// 创建并保存文件
+	err = excommons.CreateFile(filePath, fileName, bs)
+	if err != nil {
+		return
+	}
+	// Minio相关
+	if otherFunc != nil {
+		otherFunc()
+	}
+	// 从Excel文件中读取数据
+	rows, err = LoadLadderExcel(filePath, fileName, len(tableHead), ignoreRows, ignoreCols)
+	if err != nil {
+		return
+	}
+	// 记录操作相关信息
+	fileRcd.ID = excommons.UUID()
+	fileRcd.CreateTime = time.Now().Format("2006-01-02 15:04:05")
+	fileRcd.FileName = fileName
+	fileRcd.FilePath = filePath
+	fileRcd.Operation = exconst.IMPORT
+	fileRcd.FileFormat = exconst.EXCEL
+	fileRcd.FunModule = funModule
+	fileRcd.CreateUserId = userId
+	if e.DB != nil {
+		// 单例初始化数据表
+		err = once.Do(func() error {
+			return e.DB.AutoMigrate(&exmodel.FileRecord{})
+		})
+		if err != nil {
+			err = errors.New("初始化数据导入表失败：" + err.Error())
+			return
+		}
+		err = e.DB.Create(&fileRcd).Error
+		if err != nil {
+			err = errors.New("数据库异常：" + err.Error())
+			return
+		}
+	}
+	return
+}
+
 // LoadExcelByStruct
 /**
  *  @Description: 导入Excel数据，通过结构体
